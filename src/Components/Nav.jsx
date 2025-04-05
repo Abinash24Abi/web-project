99f9cee818b99bda79cf64d35663f2312aaccfc8
Abinash <127226250+Abinash24Abi@users.noreply.github.com>
2025-04-05 17:23:36 +0530

    project created

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className='nav'>
+
+        <div className="logo">
+            logo
+        </div>
+        
+        <div className='nav_menulist'>
+            <ul className='nav_ul'>
+
+                <li>Home</li>
+                <li>Add</li>
+                <li>Contact</li> 
+                <li>Contact</li>
+
+            </ul>
+        </div>
+
+      </nav>
+    </div>
+  )
+}
+
+export default Nav
